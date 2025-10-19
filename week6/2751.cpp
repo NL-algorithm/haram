4ccb2c3ecc9558f2e6a014993e35eeea022dbf97
dnjsgkfka <haribo101@seoultech.ac.kr>
2025-10-19 17:21:24 +0900

    week6-251019

---
+++ week6/2751.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N, num;
+    cin >> N;
+
+    vector<int> vec;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> num;
+        vec.push_back(num);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << vec[i] << '\n';
+    }
+}
---
+++ week6/5522.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int res = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        int n;
+        cin >> n;
+        res += n;
+    }
+    cout << res;
+}
